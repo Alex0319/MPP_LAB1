9e8054ad56df80eca8ceab28c3b4c31db78fdbde
Alex0319 <alexandrsavchuk.97@gmail.com>
2016-09-16 18:56:52 +0300

    first commit

---
+++ MPP_LAB1/QuickSort_UnitTest/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort;
+using BenchmarkDotNet;
+using BenchmarkDotNet.Attributes;
+
+namespace QuickSort_UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        
+    }
+    
+    public class My
+    {
+        [Benchmark]
+        public void Hello()
+        {
+ 
+        }
+    }
+}
