84ad1bf6a2b6dfdac1dcad575df9c960ef9e497b
Alex0319 <alexandrsavchuk.97@gmail.com>
2016-09-18 22:44:24 +0300

    create quick sort

--- MPP_LAB1/QuickSort_UnitTest/UnitTest.cs
+++ MPP_LAB1/QuickSort_UnitTest/UnitTest.cs
@@ -12,8 +12,8 @@ namespace QuickSort_UnitTest
         [TestMethod]
         public void TestMethod_CheckSortArrayWithSixElements()
         {
-            int[] array = new int[] {20,-10,16,32,11,7 };
-            int [] sortedArray=new int[] {-10,7,11,16,20,32};
+            int[] array = new int[] {20,-10,15,32,11,7 };
+            int [] sortedArray=new int[] {-10,7,11,15,20,32};
             var qSort = new QuickSort.QuickSort();
             qSort.quickSort(array, 0, array.Length-1);
             CollectionAssert.AreEqual(sortedArray,array);
